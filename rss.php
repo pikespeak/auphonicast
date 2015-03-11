232628eaac17dc57987222cb0f7c07d1d51f46c0
Volker Tanger (wyae) <volker.tanger_git@wyae.de>
2015-03-11 08:00:03 +0100

    rss now working with chooseable media type

--- rss.php
+++ rss.php
@@ -1,8 +1,8 @@
-<?php echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"; ?>
+<?php echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"; $mediatype = preg_replace("/[^A-Za-z0-9_.]/", "", $_GET["typ"]); if (""==$mediatyp){$mediatyp="mp3";} ?>
 <rss version="2.0">
 <channel>
-<title>ENTER title here - podcast - MP3 feed</title>
-<link>ENTER url of the feed here /rss.php?media=mp3</link>
+<title>ENTER title here - podcast - <?php echo $mediatype; ?> feed</title>
+<link>ENTER url of the feed here /rss.php?typ=$mediatyp</link>
 <description><![CDATA[
         ENTER description here
 ]]></description>
@@ -10,13 +10,12 @@
 <language>de</language>
 
 <?php
-include("renderauphonic.php");
+include("auphonicast/renderauphonic.php");
 
-$episodetemplate = "<item> <title><![CDATA[*title*]]></title><link>ENTER url of page here?podcast=*title*</link><guid>ENTER url of page here?podcast=*title*</guid><pubDate>*date_rfc822*</pubDate><description><![CDATA[ *date* <br />*summary* ]]></description> *MEDIA*</item>";
+$episodetemplate = "<item> <title><![CDATA[*title*]]></title><link>ENTER url of page here/?podcast=*title*</link><guid>ENTER url of page here/?podcast=*title*</guid><pubDate>*date_rfc822*</pubDate><description><![CDATA[ *date* <br />*summary* ]]></description> *MEDIA*</item>";
 $mediatemplate = "<enclosure type=\"audio/*EXT*\" url=\"*URL*?src=rss\" length=\"*BYTES*\" />";
 
 RenderAuphonic("../podcasts/", "http://url.to.auphonic/directory/", ".", ".mp3", $episodetemplate, $mediatemplate);
 ?>
-
 
 </channel>
